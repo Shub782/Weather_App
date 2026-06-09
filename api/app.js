4eac4aa7e23a779644f797b7a6bd4d2e39d0bda1
Shubham Prakash Aranha <shubhamaranha@gmail.com>
2026-06-09 12:45:05 +0530

    function change

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"../views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static,path.join(__dirname,"../public"));
+app.use(express.static,path.join(__dirname,"../public")));
 
 app.get('/', (request, response) => {
     response.render("index", {
